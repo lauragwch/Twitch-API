05f70f8415ae559e667d2eb2d23d85d82989eaff
IgorVq <igor.verquain@gmail.com>
2025-02-25 10:16:46 +0100

    adding route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Twitch API</h1>
+      <BrowserRouter>
+        {/* <NavBar /> */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const HomePage = () => {
+    return <>
+        <Container>
+            <h1>HomePage</h1>
+        </Container>
+    </>;
+}
+ 
+export default HomePage;
